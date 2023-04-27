b0b7ed6e21d2990c510691e2724c242c69c8298e
GioviZF <100273806+GioviZF@users.noreply.github.com>
2023-04-27 09:30:44 +0200

    Add files via upload

---
+++ calculadora.js
@@ -0,0 +1,136 @@
+function Display() {
+    this.displayValorActual = displayValorActual;
+    this.displayValorAnterior = displayValorAnterior;
+    this.calculador = new Calculadora();
+    this.tipoOperacion = undefined;
+    this.valorActual = '';
+    this.valorAnterior = '';
+    this.signos = {
+        sumar: '+',
+        dividir: '%',
+        multiplicar: 'x',
+        restar: '-'
+    }
+}
+
+Display.prototype.borrar = function () {
+    this.valorActual = this.valorActual.toString().slice(0, -1);
+    this.imprimirValores();
+};
+
+Display.prototype.borrarTodo = function () {
+    this.valorActual = '';
+    this.valorAnterior = '';
+    this.tipoOperacion = undefined;
+    this.imprimirValores();
+};
+
+Display.prototype.computar = function (tipo) {
+    this.tipoOperacion !== 'igual' && this.calcular();
+    this.tipoOperacion = tipo;
+    this.valorAnterior = this.valorActual || this.valorAnterior;
+    this.valorActual = '';
+    this.imprimirValores();
+};
+
+Display.prototype.agregarNumero = function (numero) {
+    if (numero === '.' && this.valorActual.includes('.')) return
+    this.valorActual = this.valorActual.toString() + numero.toString();
+    this.imprimirValores();
+};
+
+Display.prototype.imprimirValores = function () {
+    this.displayValorActual.textContent = this.valorActual;
+    this.displayValorAnterior.textContent = `${this.valorAnterior} ${this.signos[this.tipoOperacion] || ''}`;
+};
+
+Display.prototype.calcular = function () {
+    const valorAnterior = parseFloat(this.valorAnterior);
+    const valorActual = parseFloat(this.valorActual);
+
+    if (isNaN(valorActual) || isNaN(valorAnterior)) return
+    this.valorActual = this.calculador[this.tipoOperacion](valorAnterior, valorActual);
+};
+
+function Calculadora() {
+}
+Calculadora.prototype.sumar = function (num1, num2) {
+    return num1 + num2;
+};
+
+Calculadora.prototype.restar = function (num1, num2) {
+    return num1 - num2;
+};
+
+Calculadora.prototype.dividir = function (num1, num2) {
+    return num1 / num2;
+};
+
+Calculadora.prototype.multiplicar = function (num1, num2) {
+    return num1 * num2;
+};
+
+Calculadora.prototype.coseno = function (num1) {
+    return Math.cos(num1);
+}
+
+Calculadora.prototype.seno = function (num1) {
+    return Math.sin(num1);
+}
+
+Calculadora.prototype.tangente = function (num1) {
+    return Math.tan(num1);
+}
+
+Calculadora.prototype.arcotangente = function (num1) {
+    return Math.atan(num1);
+}
+
+Calculadora.prototype.raizc = function (num1) {
+    return Math.sqrt(num1);
+}
+
+var displayValorAnterior = document.getElementById('valor-anterior');
+var displayValorActual = document.getElementById('valor-actual');
+var botonesNumeros = document.querySelectorAll('.numero');
+var botonesOperadores = document.querySelectorAll('.operador');
+
+var display = new Display(displayValorAnterior, displayValorActual);
+ var calculadora=new Calculadora();
+botonesNumeros.forEach(boton => {
+    boton.addEventListener('click', () => display.agregarNumero(boton.innerHTML));
+});
+
+botonesOperadores.forEach(boton => {
+    boton.addEventListener('click', () => display.computar(boton.value))
+});
+
+ function cosenoHTML(){
+    var num=document.getElementById("valor-actual").innerText;
+    displayValorActual.innerText=calculadora.coseno(num);
+    displayValorAnterior.innerText="cos("+num+")";
+}
+
+function senoHTML(){
+    var num=document.getElementById("valor-actual").innerText;
+    displayValorActual.innerText=calculadora.seno(num);
+    displayValorAnterior.innerText="sen("+num+")";
+}
+
+function arcotangenteHTML(){
+    var num=document.getElementById("valor-actual").innerText;
+    displayValorActual.innerText=calculadora.arcotangente(num);
+    displayValorAnterior.innerText="atan("+num+")";
+}
+
+function tangenteHTML(){
+    var num=document.getElementById("valor-actual").innerText;
+    displayValorActual.innerText=calculadora.tangente(num);
+    displayValorAnterior.innerText="tan("+num+")";
+}
+
+function raizCHTML(){
+    var num=document.getElementById("valor-actual").innerText;
+    displayValorActual.innerText=calculadora.raizc(num);
+    displayValorAnterior.innerText="√("+num+")";
+}
